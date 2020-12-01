f33e7b59182a00fe22032aa1fe630138a4ad3d70
Shriram Balaji <shrirambalaji1996@gmail.com>
2020-12-01 09:31:43 +0530

    Add --copy, -c option to copy color to clipboard
    * Update Cargo.lock

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,9 @@
 use cli_hex_rgb::*;
+use copypasta::{ClipboardContext, ClipboardProvider};
 use owo_colors::OwoColorize;
 use std::process;
 use structopt::StructOpt;
+
 const SQUARE: &str = "■";
 
 #[derive(StructOpt, Debug)]
@@ -12,14 +14,21 @@ struct Opt {
     debug: bool,
 
     hex_code: String,
+
+    #[structopt(short, long)]
+    /// Enable copy to clipboard
+    copy: bool,
+}
+
+fn copy_to_clipboard(contents: &str) {
+    let mut ctx = ClipboardContext::new().unwrap();
+    ctx.set_contents(contents.to_owned()).unwrap();
 }
 
 pub fn init() {
     let opt = Opt::from_args();
     match convert_hexcode_to_rgb(opt.hex_code) {
         Ok(color) => {
-            // TODO: Highlight result with specified color, or show a box with filled with color
-            // TODO: Add option -c, --copy to copy rgb value to clipboard
             // TODO: Add option -i, --inverse to convert from rgb to hex
             let rgb = format!("rgb({}, {}, {})", color.red, color.green, color.blue);
             let output = format!(
@@ -31,6 +40,11 @@ pub fn init() {
 
             println!("{}", output);
 
+            if opt.copy {
+                copy_to_clipboard(&rgb);
+                println!("{}", "copied to your clipboard.".dimmed())
+            }
+        }
         Err(e) => {
             eprintln!("{} {}", " ERROR ".bold().on_bright_red().bright_white(), e.bright_red());
             process::exit(1);
